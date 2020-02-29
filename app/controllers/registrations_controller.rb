ae04b765b38b656fdaa2bdb0670cc663b5becbd9
studix <marco.studer@gmx.net>
2020-02-29 21:48:04 +0100

    validation error styling

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -55,11 +55,11 @@ class RegistrationsController < ApplicationController
    # DELETE /registrations/1
    # DELETE /registrations/1.json
    def destroy
-     @registration = Registration.find(registration_params[:id])
+     @registration = Registration.find(params[:id])
      @registration.destroy
  
      respond_to do |format|
-       format.html { redirect_to registrations_url }
+       format.html { redirect_to tournament_registrations_url }
        format.json { head :no_content }
      end
    end
