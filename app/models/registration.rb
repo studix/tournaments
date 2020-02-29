7c30035faee0d07b6afd494535f5cc4931c5f166
studix <marco.studer@gmx.net>
2020-02-29 22:16:19 +0100

    add table styling

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,5 @@
 class Registration < ApplicationRecord
     belongs_to :tournament
     has_and_belongs_to_many :draws
-    validates_presence_of :name, :first_name, :email, :tournament_id, :phone, :message => " ist obligatorisch."
+    validates_presence_of :name, :first_name, :email, :tournament_id => " ist obligatorisch."
 end
