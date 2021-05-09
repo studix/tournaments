86fee4f75089018cc09faeb1cada077a79e0d745
studix <marco.studer@gmx.net>
2021-05-09 21:46:35 +0200

    change sendgrid to api key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net" ,
-    :port => 465,
+    :port => 587,
     :domain => "tcdulliken.ch" ,
     :tls => true,
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'] ,
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
     :enable_starttls_auto => true
   }
 
