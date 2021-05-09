dba4172effeafbf079b50ef771c14d078e92e167
studix <marco.studer@gmx.net>
2021-05-09 23:57:34 +0200

    change port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net" ,
-    :port => 587,
+    :port => 465,
     :domain => "tcdulliken.ch" ,
     :tls => true,
     :authentication => :plain,
