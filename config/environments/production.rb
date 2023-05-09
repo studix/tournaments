a94a66d05c8777f5ad6b36f4b8d74c47a9da1732
studix <marco.studer@gmx.net>
2023-05-09 20:30:21 +0200

    re-add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tournaments_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    :address => "smtp.sendgrid.net" ,
+    :port => 465,
+    :domain => "tcdulliken.ch" ,
+    :tls => true,
+    :authentication => :plain,
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
